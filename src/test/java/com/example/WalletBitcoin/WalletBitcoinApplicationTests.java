2ecd5c848e67d3c6d81c8e9963e0bc2482f384bd
Jhon Ccamerccoa Perez <jccamerc@emeal.nttdata.com>
2022-03-23 12:30:33 -0500

    first commit

---
+++ src/test/java/com/example/WalletBitcoin/WalletBitcoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WalletBitcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletBitcoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
